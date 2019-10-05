403015a8990670297e7ef6c31835eb1449b1357e
TenjoViolet@gmail.com <TenjoViolet@gmail.com>
2019-10-05 15:44:39 +0800

    新增第三個測試案例CheckLoginAccountLegalTest_輸入正確帳號且輸入正確密碼_應回傳true()

--- TDDUnitTestLoginTests/Service/LoginTests.cs
+++ TDDUnitTestLoginTests/Service/LoginTests.cs
@@ -61,6 +61,28 @@ namespace TDDUnitTestLogin.Service.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+        public void CheckLoginAccountLegalTest_輸入正確帳號且輸入正確密碼_應回傳true()
+        {
+            //arrange準備受測物件、參數、預期結果
+            List<Account> ltAccounts = new List<Account>
+            {
+                new Account{ UserId="admin", Pwd="123456"},
+                new Account{ UserId="Jack", Pwd="123456"},
+                new Account{ UserId="Rose", Pwd="123456"},
+            };
+            IAccountRepository accountRepository = Substitute.For<IAccountRepository>();
+            Login login = new Login(accountRepository);
+            accountRepository.SelectAccounts().Returns(ltAccounts);
+
+            Boolean expected = true;
+
+            //act執行受測方法
+            Boolean actual = login.CheckLoginAccountLegal("Jack", "123456");
+
+            //assert驗證執行結果與預測結果是否一致
+            Assert.AreEqual(expected, actual);
+        }
 
     }
 }
